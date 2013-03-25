ea783874d5201fdfd8d666bd4c8a4ed82eca0e12
Chongmyung Park <chongmyungpark@gmail.com>
2013-03-25 16:51:05 +0900

    add missing files

---
+++ adm/gpf/_common.php
@@ -0,0 +1,8 @@
+<?
+define('G4_IS_ADMIN', true);
+$g4_path = "../.."; // common.php 의 상대 경로
+include_once ("$g4_path/common.php");
+include_once(G4_ADMIN_PATH."/admin.lib.php");
+$sub_menu = "717000";
+
+?>
